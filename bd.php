8a51824b232778ae075e042741e0cef560e7ce65
alina2012 <lina.alina2012.kalinina@mail.ru>
2020-04-19 13:21:49 +0300

    Modifying Database data

--- bd.php
+++ bd.php
@@ -1,7 +1,7 @@
 <?php
-	define("DB_SERVER", "localhost:3308");
-	define("DB_USER", "root");
-	define("DB_PASS", "");
+	define("DB_SERVER", "localhost:3306");
+	define("DB_USER", "admin");
+	define("DB_PASS", "pass");
 	define("DB_NAME", "shop");
 	$db = mysqli_connect (DB_SERVER,DB_USER,DB_PASS, DB_NAME);
 	$max = mysqli_query($db, "SELECT MAX(update_id) as update_id FROM bot_mes");
