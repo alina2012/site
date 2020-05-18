de8563ad003087fb6075cbe65c953e04e88b2037
alina2012 <lina.alina2012.kalinina@mail.ru>
2020-05-18 16:55:10 +0300

    Adding functions

--- src/Classes/Models/DB.php
+++ src/Classes/Models/DB.php
@@ -45,4 +45,63 @@ class DB
         }
       }
     }
+  /**
+  * Function to add an entry to the Bot_mes table
+  * @param $db Db connection
+  * @param $chat_id User chat ID
+  * @param $update_id The number of the last processed request
+  * @param $text Message text
+  *
+  * @return FALSE or TRUE
+  */
+    public function insertRecordintoBot_mes($db, $chat_id, $update_id, $text)
+    { $numargs = func_num_args();
+      if($numargs > 4){
+        return FALSE;
+      }
+      $record = mysqli_query($db, "INSERT INTO bot_mes (chat_id, update_id, text_mes) VALUES ('$chat_id','$update_id', '$text')");
+        return $record;
+    }
+  /**
+  * Function to add an entry to the users table
+  * @param $db Db connection
+  * @param $name User name
+  * @param $login User email
+  * @param $salt HASH salt
+  * @param $password User pessword
+  * @param $token Authorization token
+  * @param $phone User phone
+  *
+  * @return FALSE or TRUE
+  */
+    public function insertRecordintoUsers($db, $name, $login, $salt, $password, $token, $phone)
+    { $numargs = func_num_args();
+      if($numargs > 7){
+        return FALSE;
+      }
+      $record = mysqli_query($db, "INSERT INTO users (name, login, salt, password, token, phone) VALUES ('$name','$login', '$salt', '$password', '$token', '$phone')");
+        return $record;
+    }
+  /**
+  * Function to add an entry to the order_table table
+  * @param $db Db connection
+  * @param $order_id Order ID
+  * @param $email User email
+  * @param $my_json Order list
+  *
+  * @return FALSE or TRUE
+  */
+    public function insertRecordintoOrder_table($db, $order_id, $email, $my_json)
+    { $numargs = func_num_args();
+      if($numargs > 4){
+        return FALSE;
+      }
+      $result = json_decode($my_json);
+      if (json_last_error() === JSON_ERROR_NONE) {
+          $record = mysqli_query($db, "INSERT INTO order_table (order_id, email, composition, status) VALUES ('$order_id','$email', '$my_json', 1)");
+        return $record;
+      } else {
+        return FALSE;
+      }
+    }
 }
