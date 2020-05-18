9cf0a0cf602e8d1253b1a8a5c0f2fd7490519f7c
alina2012 <lina.alina2012.kalinina@mail.ru>
2020-05-18 18:50:36 +0300

    Create dump database

--- shop.sql
+++ shop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3308
--- Время создания: Май 15 2020 г., 15:23
+-- Время создания: Май 18 2020 г., 15:47
 -- Версия сервера: 8.0.18
 -- Версия PHP: 7.3.12
 
@@ -35,7 +35,17 @@ CREATE TABLE IF NOT EXISTS `bot_mes` (
   `update_id` varchar(200) NOT NULL,
   `text_mes` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `bot_mes`
+--
+
+INSERT INTO `bot_mes` (`id`, `chat_id`, `update_id`, `text_mes`) VALUES
+(18, '1', '1', '1'),
+(17, '1', '1', '1'),
+(16, '1', '1', '1'),
+(15, '1', '1', '1');
 
 -- --------------------------------------------------------
 
@@ -79,7 +89,7 @@ CREATE TABLE IF NOT EXISTS `order_table` (
   `composition` json NOT NULL,
   `status` int(10) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `order_table`
@@ -87,7 +97,11 @@ CREATE TABLE IF NOT EXISTS `order_table` (
 
 INSERT INTO `order_table` (`id`, `order_id`, `email`, `composition`, `status`) VALUES
 (1, '239744097', 'alice123_234@mail.ru', '{\"2\": {\"price1\": \"180\", \"quantity1\": 4}}', 3),
-(2, '326541358', 'alice123_234@mail.ru', '{\"2\": {\"price2\": \"210\", \"quantity2\": 1}, \"3\": {\"price1\": \"190\", \"quantity1\": \"2\"}}', 1);
+(2, '326541358', 'alice123_234@mail.ru', '{\"2\": {\"price2\": \"210\", \"quantity2\": 1}, \"3\": {\"price1\": \"190\", \"quantity1\": \"2\"}}', 3),
+(17, '2', '3', '{}', 1),
+(16, '2', '3', '{}', 1),
+(15, '2', '3', '{}', 1),
+(14, '2', '3', '{}', 1);
 
 -- --------------------------------------------------------
 
@@ -116,6 +130,26 @@ INSERT INTO `status_description` (`id`, `num`, `description`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Структура таблицы `test`
+--
+
+DROP TABLE IF EXISTS `test`;
+CREATE TABLE IF NOT EXISTS `test` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(256) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `test`
+--
+
+INSERT INTO `test` (`id`, `name`) VALUES
+(2, '12234');
+
+-- --------------------------------------------------------
+
+--
 -- Структура таблицы `users`
 --
 
@@ -129,15 +163,18 @@ CREATE TABLE IF NOT EXISTS `users` (
   `token` varchar(256) NOT NULL,
   `phone` varchar(256) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=24 DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `users`
 --
 
 INSERT INTO `users` (`id`, `name`, `login`, `salt`, `password`, `token`, `phone`) VALUES
-(3, 'Администратор', 'lina.alina2012.kalinina@mail.ru', '60fbb179446d962bfec2', '1594e5a2a6f38c312c4670edfc38f8791447468223c7319476f8c16f55b9260e', '200aad748ee48eb0783c43d029e73dd76edc4d1759c66498d233ecd05ba8f40a', '89118257284'),
-(4, 'Иванов Иван Владимирович', 'alice123_234@mail.ru', 'acbf84a4d145e836285b', 'c69a0509018389841b51f6d6dce6ce128d06ba5b19d09614a75620b6b973822a', 'da4ab8f8ceb0eed8e2045c481c2815be0cf153503d929db6a1f90f34111eed66', '89969596327');
+(3, 'Администратор', 'lina.alina2012.kalinina@mail.ru', '60fbb179446d962bfec2', '1594e5a2a6f38c312c4670edfc38f8791447468223c7319476f8c16f55b9260e', '54d6fecdb8edf097fec8243f0c56bd1b831a453b5528ce831a68ad320e21b68f', '89118257284'),
+(4, 'Иванов Иван Петрович', 'alice123_234@mail.ru', 'acbf84a4d145e836285b', 'c69a0509018389841b51f6d6dce6ce128d06ba5b19d09614a75620b6b973822a', 'df3fee345c36cedf289765b6cf30a35460fe7c2f8ccdee5eefb9363bb485bce8', '89969596327'),
+(21, '2', '3', '4', '5', 'string', 'phone'),
+(22, '2', '3', '4', '5', 'string', 'phone'),
+(23, '2', '3', '4', '5', 'string', 'phone');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
